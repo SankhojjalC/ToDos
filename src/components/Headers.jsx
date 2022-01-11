eb75a3d73a69f45bce642cbcf42c9555bd94d02f
SankhojjalChatterjee <sankhojjal.chatterjee@publicissapient.com>
2022-01-12 00:08:58 +0530

    added redux toolkit setup

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -4,7 +4,7 @@ export const Headers = () => {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="profile">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto"></Nav>
