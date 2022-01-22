592fe0823d77eddceff47c4abc9c8463d5419dd0
SankhojjalChatterjee <sankhojjal.chatterjee@publicissapient.com>
2022-01-23 02:05:58 +0530

    fixed routing issue

--- src/utils/PrivateRoute.js
+++ src/utils/PrivateRoute.js
@@ -1,13 +1,11 @@
 import { Redirect, Route } from "react-router-dom";
-import { useSelector } from "react-redux";
 
 export const PrivateRoute = ({ component: Component, ...rest }) => {
-  const isloggedIn = useSelector((state) => state.logedInReducer.isloggedIn);
   return (
     <Route
       {...rest}
       render={(props) =>
-        isloggedIn ? (
+        localStorage.getItem("loginUserDetails") ? (
           <Component {...props} />
         ) : (
           <Redirect to={{ pathname: "/", state: { from: props.location } }} />
